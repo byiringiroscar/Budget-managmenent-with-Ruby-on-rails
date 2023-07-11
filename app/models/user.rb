d115b18f1c133e1d8f5787995beec479afb1aecf
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-07-11 13:13:43 +0200

    Add category models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+  has_many :spends, class_name: "spend", foreign_key: "reference_id"
+  has_many :categories, class_name: "category", foreign_key: "reference_id"
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_10_205632) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_10_213218) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.string "icon"
+    t.bigint "author_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_categories_on_author_id"
+  end
 
   create_table "spends", force: :cascade do |t|
     t.string "name"
@@ -36,5 +45,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_10_205632) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "categories", "users", column: "author_id"
   add_foreign_key "spends", "users", column: "author_id"
 end
