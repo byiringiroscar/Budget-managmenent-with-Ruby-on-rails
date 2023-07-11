052d17b60c41d77ff51d479dc41fe90c9f80a094
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-07-11 19:37:36 +0200

    Add testing for Spend model

--- spec/models/spend_spec.rb
+++ spec/models/spend_spec.rb
@@ -1,32 +1,39 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe Spend, type: :model do
-#     subject do
-#         Spend.new(
-#             name: "Food",
-#             amount: 100,
-#             author_id: 1,
-#         )
-#     end
+RSpec.describe Spend, type: :model do
+    subject do
+        Spend.new(
+            name: "Cake",
+            amount: 100,
+            author_id: 1,
+            category_id: 1,
+        )
+    end
 
-#     before { subject.save }
+    before { subject.save }
 
-#     it "is valid with valid attributes" do
-#         expect(subject).to be_valid
-#     end
+    it "is valid with valid attributes" do
+        expect(subject).to be_valid
+    end
 
-#     it "is not valid without a name" do
-#         subject.name = nil
-#         expect(subject).to_not be_valid
-#     end
+    it "is not valid without a name" do
+        subject.name = nil
+        expect(subject).to_not be_valid
+    end
 
-#     it "is not valid without a amount" do
-#         subject.amount = nil
-#         expect(subject).to_not be_valid
-#     end
+    it "is not valid without a amount" do
+        subject.amount = nil
+        expect(subject).to_not be_valid
+    end
 
-#     it "is not valid without a author_id" do
-#         subject.author_id = nil
-#         expect(subject).to_not be_valid
-#     end
-# end
+    it "is not valid without a author_id" do
+        subject.author_id = nil
+        expect(subject).to_not be_valid
+    end
+
+    it "is not valid without a category_id" do
+        subject.category_id = nil
+        expect(subject).to_not be_valid
+    end
+    
+end
