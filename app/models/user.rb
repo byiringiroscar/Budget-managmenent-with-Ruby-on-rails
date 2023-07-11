8c3a2b2abe9c7f97f413d39570e0cabcae9a33e1
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-07-11 19:25:49 +0200

    User model validation and testing

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,11 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 
-  has_many :spends, class_name: "spend", foreign_key: "reference_id"
-  has_many :categories, class_name: "category", foreign_key: "reference_id"
+  validates :full_name, presence: true 
+  validates :email, uniqueness: { case_sensitive: false }
+
+  has_many :spends, class_name: "spend", foreign_key: "author_id"
+  has_many :categories, class_name: "category", foreign_key: "author_id"
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+
+RSpec.describe User, type: :model do
+    subject do
+        User.new(
+            full_name: "Geno",
+            email: "geno@gmail.com",
+            password: "123456",
+        )
+    end
+
+    before { subject.save }
+
+    it "is valid with valid attributes" do
+        expect(subject).to be_valid
+    end
+
+    it "is not valid without a full_name" do
+        subject.full_name = nil
+        expect(subject).to_not be_valid
+    end
+
+    it "is not valid without a email" do
+        subject.email = nil
+        expect(subject).to_not be_valid
+    end
+end
