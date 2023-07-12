16c683cfc0f932b7c350e158293e0069a97de2a5
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-07-12 23:26:12 +0200

    Add registration and login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,26 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validate :password_confirmation_match
+
+  # def password_confirmation_match
+  # if password != password_confirmation
+  #   puts "====================================================="
+  #   puts password
+  #   puts password_confirmation
+  #   puts "==============================================================="
+  #   errors.add(:password_confirmation, "must match the password")
+  # end
+  # end
 
   validates :full_name, presence: true
   validates :email, uniqueness: { case_sensitive: false }
 
+
   has_many :spends, class_name: 'Spend', foreign_key: 'author_id'
   has_many :categories, class_name: 'Category', foreign_key: 'author_id'
 
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
 end
