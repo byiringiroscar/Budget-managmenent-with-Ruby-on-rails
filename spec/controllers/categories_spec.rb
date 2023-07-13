dc495e5390f55404db935b5ecbd9584996e6051f
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-07-13 23:07:18 +0200

    Add test for splash

---
+++ spec/controllers/categories_spec.rb
@@ -0,0 +1,12 @@
+# require 'rails_helper'
+
+# RSpec.describe CategoriesController, type: :request do
+#     describe CategoriesController do
+#         describe "#index" do
+#             it 'returns the correct response status' do
+#                 get :index
+#                 expect(response.status).to eq 200
+#             end
+#         end
+#     end
+# end
